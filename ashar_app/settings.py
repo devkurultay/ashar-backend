a00816837a793a38e3f59631d875aa6ede445813
Alymbekov <alymbekovdastan1@gmail.com>
2021-03-14 00:55:49 +0600

    add settings gunicorn nginx for deploy

--- ashar_app/settings.py
+++ ashar_app/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost', '127.0.0.1', 'ashar-backend.ga', '130.61.93.7'
+]
 
 
 # Application definition
