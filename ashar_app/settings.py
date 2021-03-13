3a1aa7dbf9e8ed6811bbcbe87f2916c7d8a2cc0e
Alymbekov <alymbekovdastan1@gmail.com>
2021-03-14 02:26:26 +0600

    add new ip address

--- ashar_app/settings.py
+++ ashar_app/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = [
-    'localhost', '127.0.0.1', 'ashar-backend.ga', '130.61.93.7'
+    'localhost', '127.0.0.1', 'ashar-backend.ga', '130.61.93.7', '164.90.171.209'
 ]
 
 
