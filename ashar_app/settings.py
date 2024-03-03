47bd3f63c100230f297c887af96d99295d017b72
Kalys Osmonov <kalys@osmonov.com>
2024-03-03 21:04:00 +0100

    Update cors package

--- ashar_app/settings.py
+++ ashar_app/settings.py
@@ -61,13 +61,19 @@ INSTALLED_APPS = [
 
 ]
 SITE_ID = 1
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = True
+
+# CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
