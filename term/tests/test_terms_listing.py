0544c889878d7325037dbfbfd36200daca54002f
Kalys Osmonov <kalys@osmonov.com>
2024-04-22 22:03:26 +0200

    Add tests for searching by term and description

--- term/tests/test_terms_listing.py
+++ term/tests/test_terms_listing.py
@@ -47,10 +47,50 @@ class TermListingTestCase(APITestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(json.loads(response.content), expected_response)
 
-    @unittest.skip
     def test_search_by_term(self):
-        print('not implemented')
+        second_term = factories.TermFactory(term='процессорлор', category=self.term.category)
+        response = self.client.get('/api/v1/terms/?search=процессор')
 
-    @unittest.skip
+        expected_response = [
+            {
+                'category': {'slug': 'programmaloo', 'title': 'Программалоо'},
+                'description': self.term.description,
+                'id': self.term.id,
+                'other_language_examples': [],
+                'term': 'процессор',
+                'translation_suggestions': []
+            },
+            {
+                'category': {'slug': 'programmaloo', 'title': 'Программалоо'},
+                'description': second_term.description,
+                'id': second_term.id,
+                'other_language_examples': [],
+                'term': 'процессорлор',
+                'translation_suggestions': []
+            }
+        ]
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.content), expected_response)
+
     def test_search_by_description(self):
-        print('not implemented')
+        second_term = factories.TermFactory(
+                term='cpu',
+                description='компьютердеги процессор',
+                category=self.term.category
+        )
+        response = self.client.get('/api/v1/terms/?search=компьютер')
+
+        expected_response = [
+            {
+                'category': {'slug': 'programmaloo', 'title': 'Программалоо'},
+                'description': second_term.description,
+                'id': second_term.id,
+                'other_language_examples': [],
+                'term': 'cpu',
+                'translation_suggestions': []
+            }
+        ]
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.content), expected_response)
